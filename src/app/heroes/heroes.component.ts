bae98b5195423e4eee69bfd540d4794b3569610a
Meir Chechik <meirchechik2015@gmail.com>
2024-02-19 20:42:10 +0200

    feat: added heroes list, add hero select, add list style, used ngFor and ngIf

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,8 +12,10 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  selectedHero?: Hero;
+  heroes: Hero[] = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
