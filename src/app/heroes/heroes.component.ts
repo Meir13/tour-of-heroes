294088a76837132896db3984f0518ff452f4a9e3
Meir Chechik <meirchechik2015@gmail.com>
2024-03-09 10:28:33 +0200

    feat: refactor heroes component to two components hero-detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
+  imports: [CommonModule, FormsModule, HeroDetailComponent],
 })
 export class HeroesComponent {
   selectedHero?: Hero;
